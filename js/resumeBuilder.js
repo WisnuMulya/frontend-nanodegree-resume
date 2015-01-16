b9863c0148f6705d60136af2b8fd1142b5a915dd
Wisnu Mulya <wisnu.p.mulya@gmail.com>
2015-01-17 02:39:46 +0700

    Add work object to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -16,3 +16,16 @@ var bio = {
     ], //end of skills
     "biopic": "http://www.gravatar.com/avatar/c076638bbeb514e5d4f6d2fa04bb8d23?s=200"
 }; //end of bio
+
+//create work object
+var work = {
+    "jobs": [
+        {
+            "employer": "Zenius Education",
+            "title": "ZeniusEnglish Manager",
+            "location": "Jakarta, Indonesia",
+            "dates": "June 2012 - Current",
+            "description": "Starting up a new business unit for the company and mainly responsible in managing daily operational routines, orienting on customer development or lean startup process, engaging on inbound marketing, doing front-end web development, and making materials for online teaching."
+        }
+    ] //end of jobs array
+}; //end of work
