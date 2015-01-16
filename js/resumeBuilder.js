fba045c51f8d1830eeeac344d9ac721e662e1688
Wisnu Mulya <wisnu.p.mulya@gmail.com>
2015-01-17 02:32:40 +0700

    Add bio object to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,18 @@
+//create bio object
+var bio = {
+    "name": "Wisnu Mulya",
+    "role": "Frontend Developer",
+    "contacts": {
+        "mobile": "555-5555",
+        "email": "wisnu@wisnumulya.com",
+        "github": "WisnuMulya",
+        "twitter": "@WisnuMulya",
+        "blog": "blog.wisnumulya.com",
+        "location": "Jakarta"
+    }, //end of contacts
+    "welcomeMessage":"Hi! Welcome to my resume page.",
+    "skills": [
+        "Web Development", "Startup Development", "Data Analysis", "Inbound Marketing", "Online Course Development"
+    ], //end of skills
+    "biopic": "http://www.gravatar.com/avatar/c076638bbeb514e5d4f6d2fa04bb8d23?s=200"
+}; //end of bio
